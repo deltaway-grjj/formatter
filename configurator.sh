4f28beda7f914056c4e3e51f21f8dfb5a4c6a9cd
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-08-26 13:35:23 -0300

    Update configurator.sh

--- configurator.sh
+++ configurator.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+sudo mkdir /storage
+OUTPUT="$(sudo blkid -s UUID -o value /dev/mmcblk0p3)"
+echo "UUID="$OUTPUT" /storage ext4 defaults,noatime 0 2" | sudo tee -a /etc/fstab
+sudo reboot
