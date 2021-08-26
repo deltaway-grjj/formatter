1b7e181b17e8fd90ee8bfd722369e9869cee56e5
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-08-26 11:15:57 -0300

    Update formatter.sh

--- formatter.sh
+++ formatter.sh
@@ -20,6 +20,3 @@ w
 q
 EOF
 sudo mkfs.ext4 /dev/sda3
-sudo mkdir /storage
-OUTPUT="$(sudo blkid -s UUID -o value /dev/mmcblk0p3)"
-echo "UUID="$OUTPUT" /storage ext4 defaults,noatime 0 2" | sudo tee -a /etc/fstab
