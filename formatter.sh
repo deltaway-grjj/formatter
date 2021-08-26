da0de0b99e28ed26589b700974b3352718452e10
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-08-26 10:32:01 -0300

    Update formatter.sh

--- formatter.sh
+++ formatter.sh
@@ -1,1 +1,24 @@
+sudo umount /dev/sda1
+sudo umount /dev/sda2
+sudo e2fsck -f /dev/sda2
+sudo resize2fs /dev/sda2 3G
+sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo fdisk /dev/sda
+d
+2
+n
+p
+2
+532480
++3G
+n
+p
+3
+6823936
 
+w
+q
+EOF
+sudo mkfs.ext4 /dev/sda3
+sudo mkdir /storage
+OUTPUT="$(sudo blkid -s UUID -o value /dev/mmcblk0p3)"
+echo "UUID="$OUTPUT" /storage ext4 defaults,noatime 0 2" | sudo tee -a /etc/fstab
