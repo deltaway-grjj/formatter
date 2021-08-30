7388a628ebc845bd0982ea82bfa608848b91fe5c
deltaway-grjj <75840201+deltaway-grjj@users.noreply.github.com>
2021-08-30 15:59:33 -0300

    Update writable.sh

--- writable.sh
+++ writable.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 DIR="/home/pi/deltaway/MT300"
-sudo mount --bind $DIR/Config $DIR/Config
-sudo mount --bind $DIR/F1/Backup $DIR/F1/Backup
-sudo mount --bind $DIR/F1/NaoColetadas $DIR/F1/NaoColetadas
-sudo mount --bind $DIR/F1/Teste $DIR/F1/Teste
-sudo mount --bind $DIR/F2/Backup $DIR/F2/Backup
-sudo mount --bind $DIR/F2/NaoColetadas $DIR/F2/NaoColetadas
-sudo mount --bind $DIR/F2/Teste $DIR/F2/Teste
-sudo mount --bind $DIR/F3/Backup $DIR/F3/Backup
-sudo mount --bind $DIR/F3/NaoColetadas $DIR/F3/NaoColetadas
-sudo mount --bind $DIR/F3/Teste $DIR/F3/Teste
-sudo mount --bind $DIR/F4/Backup $DIR/F4/Backup
-sudo mount --bind $DIR/F4/NaoColetadas $DIR/F4/NaoColetadas
-sudo mount --bind $DIR/F4/Teste $DIR/F4/Teste
-sudo mount --bind $DIR/Log $DIR/Log
-sudo mount -o remount,rw $DIR/Config $DIR/Config
-sudo mount -o remount,rw $DIR/F1/Backup $DIR/F1/Backup
-sudo mount -o remount,rw $DIR/F1/NaoColetadas $DIR/F1/NaoColetadas
-sudo mount -o remount,rw $DIR/F1/Teste $DIR/F1/Teste
-sudo mount -o remount,rw $DIR/F2/Backup $DIR/F2/Backup
-sudo mount -o remount,rw $DIR/F2/NaoColetadas $DIR/F2/NaoColetadas
-sudo mount -o remount,rw $DIR/F2/Teste $DIR/F2/Teste
-sudo mount -o remount,rw $DIR/F3/Backup $DIR/F3/Backup
-sudo mount -o remount,rw $DIR/F3/NaoColetadas $DIR/F3/NaoColetadas
-sudo mount -o remount,rw $DIR/F3/Teste $DIR/F3/Teste
-sudo mount -o remount,rw $DIR/F4/Backup $DIR/F4/Backup
-sudo mount -o remount,rw $DIR/F4/NaoColetadas $DIR/F4/NaoColetadas
-sudo mount -o remount,rw $DIR/F4/Teste $DIR/F4/Teste
-sudo mount -o remount,rw $DIR/Log $DIR/Log
+mount --bind $DIR/Config $DIR/Config
+mount --bind $DIR/F1/Backup $DIR/F1/Backup
+mount --bind $DIR/F1/NaoColetadas $DIR/F1/NaoColetadas
+mount --bind $DIR/F1/Teste $DIR/F1/Teste
+mount --bind $DIR/F2/Backup $DIR/F2/Backup
+mount --bind $DIR/F2/NaoColetadas $DIR/F2/NaoColetadas
+mount --bind $DIR/F2/Teste $DIR/F2/Teste
+mount --bind $DIR/F3/Backup $DIR/F3/Backup
+mount --bind $DIR/F3/NaoColetadas $DIR/F3/NaoColetadas
+mount --bind $DIR/F3/Teste $DIR/F3/Teste
+mount --bind $DIR/F4/Backup $DIR/F4/Backup
+mount --bind $DIR/F4/NaoColetadas $DIR/F4/NaoColetadas
+mount --bind $DIR/F4/Teste $DIR/F4/Teste
+mount --bind $DIR/Log $DIR/Log
+mount -o remount,rw $DIR/Config $DIR/Config
+mount -o remount,rw $DIR/F1/Backup $DIR/F1/Backup
+mount -o remount,rw $DIR/F1/NaoColetadas $DIR/F1/NaoColetadas
+mount -o remount,rw $DIR/F1/Teste $DIR/F1/Teste
+mount -o remount,rw $DIR/F2/Backup $DIR/F2/Backup
+mount -o remount,rw $DIR/F2/NaoColetadas $DIR/F2/NaoColetadas
+mount -o remount,rw $DIR/F2/Teste $DIR/F2/Teste
+mount -o remount,rw $DIR/F3/Backup $DIR/F3/Backup
+mount -o remount,rw $DIR/F3/NaoColetadas $DIR/F3/NaoColetadas
+mount -o remount,rw $DIR/F3/Teste $DIR/F3/Teste
+mount -o remount,rw $DIR/F4/Backup $DIR/F4/Backup
+mount -o remount,rw $DIR/F4/NaoColetadas $DIR/F4/NaoColetadas
+mount -o remount,rw $DIR/F4/Teste $DIR/F4/Teste
+mount -o remount,rw $DIR/Log $DIR/Log
